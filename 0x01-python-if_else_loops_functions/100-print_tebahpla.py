fc70526864b35271cc21cb6eddadf86ed56cd48c
Igbaji Joseph <zeusbenson3@gmail.com>
2023-01-04 02:21:09 -0800

    correction

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# Author - Gabriel Dan
 
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+for c in reversed(range(97, 123)):
+    if c % 2 == 1:
+        c -= 32
+    print(chr(c), end="")
